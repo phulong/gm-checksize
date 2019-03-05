134917ed6cc5b42fa03d03f0920c506849624791
phulongnls <lho@nlstech.net>
2019-03-05 15:02:53 +0700

    Update api doc:

--- doc_api.js
+++ doc_api.js
@@ -1,52 +1,15 @@
 /** **  REQUIRED API DOCS TO BE COMPATIBLE WITH KINTOHUB **
- * @api {post} /check_size/  request check information image from URL
- * @apiName CheckSize
- *
+ * @api {POST} /check_size/  request check information image from URL
+ * @apiName check_size
+ * @apiHeader (Session) {String} auth-account-id Logged in User's Id
+ * @apiHeader {String} authorization Logged in User's Id
  * @apiParam {String} image_url url of Image.
- *
- * @apiSuccess {String} [width] Width of the Image.
- * @apiSuccess {String} [height]  Height of the Image.
- *
- * @apiSuccessExample {json} [Success-Response]:
- *     HTTP/1.1 200 OK
- *     {
- *       "width": "272",
- *       "height": "92"
- *     }
- *
- * @apiError [error] ImageNotFound
- * @apiError [code] 400
- *
- * @apiErrorExample {json} [Error-Response]:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "ImageNotFound",
- *       "code": "400"
- *     }
  */
- 
  /** **  REQUIRED API DOCS TO BE COMPATIBLE WITH KINTOHUB **
-
- * @api {GET} /resize/?width=:width&height=:height  request resize image
- * @apiName Resize
- *
- * @apiParam {Number} [width] width attribute of the IMG resized.
- * @apiParam {Number} [height] height attribute of the IMG resized.
- *
- * @apiSuccessExample {json} [Success-Response]:
- *     HTTP/1.1 200 OK
- *     {
- *      "code":200,
- *      "messsage":"resize success"
- *     }
- *
- * @apiError [error] Can not resize image
- * @apiError [code] 400
- *
- * @apiErrorExample {json} [Error-Response]:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "code":"400",
- *       "error":"resize not success"
- *     }
+ * @api {GET} /resize?width=:width&height=:height  request resize image
+ * @apiName resize
+ * @apiHeader (Session) {String} auth-account-id Logged in User's Id
+ * @apiHeader {String} authorization Logged in User's Id 
+ * @apiParam {String} width width attribute of the IMG resized.
+ * @apiParam {String} height attribute of the IMG resized.
  */
