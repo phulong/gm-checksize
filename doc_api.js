11e7d7296aea16655450704a6b9f6c93d3d157f1
phulongnls <lho@nlstech.net>
2019-03-05 15:10:07 +0700

    Update api doc:

--- doc_api.js
+++ doc_api.js
@@ -1,15 +1,18 @@
 /** **  REQUIRED API DOCS TO BE COMPATIBLE WITH KINTOHUB **
  * @api {POST} /check_size/  request check information image from URL
  * @apiName check_size
- * @apiHeader (Session) {String} auth-account-id Logged in User's Id
- * @apiHeader {String} authorization Logged in User's Id
  * @apiParam {String} image_url url of Image.
+ * @apiSuccess {String} [width] Width of the Image.
+ * @apiSuccess {String} [height]  Height of the Image.
+ * @apiError [error] ImageNotFound
+ * @apiError [code] 400
  */
  /** **  REQUIRED API DOCS TO BE COMPATIBLE WITH KINTOHUB **
  * @api {GET} /resize?width=:width&height=:height  request resize image
  * @apiName resize
- * @apiHeader (Session) {String} auth-account-id Logged in User's Id
  * @apiHeader {String} authorization Logged in User's Id 
  * @apiParam {String} width width attribute of the IMG resized.
  * @apiParam {String} height attribute of the IMG resized.
+ * @apiError [error] Can not resize image
+ * @apiError [code] 400
  */
